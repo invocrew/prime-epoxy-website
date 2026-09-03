083978226e350914948bbe4651d5a10c646bcd10
husskha <h.k.h20229@gmail.com>
2026-09-03 09:45:26 -0400

    add meta pixel

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 import { FaqJsonLd } from "@/components/json-ld";
 import { LocaleProvider } from "@/components/locale-provider";
 import { SiteShell } from "@/components/site-shell";
@@ -76,6 +77,27 @@ export default async function LocaleLayout({
 
   return (
     <LocaleProvider locale={locale} dictionary={dictionary}>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2826500894396319');
+fbq('track', 'PageView');`}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=2826500894396319&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <FaqJsonLd locale={locale as Locale} />
       <SiteShell>{children}</SiteShell>
     </LocaleProvider>
